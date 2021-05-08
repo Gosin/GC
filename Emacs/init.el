3e6790b4f4c328f7a4eb3fe8057a67b9e60226cb
Gosin <canling0@gmail.com>
2021-05-09 00:06:28 +0100

    Enable rainbow-delimiter mode for most modes

--- Emacs/init.el
+++ Emacs/init.el
@@ -10,18 +10,33 @@
 (set-frame-parameter nil 'background-mode 'dark)
 (load-theme 'solarized t)
 
+;; enable rainbow-delimiter mode
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
 ;; define three useful commands(from org guide)
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c c") 'org-capture)
 
 (custom-set-variables
- ;; score apropos results by relavance.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(apropos-sort-by-scores t)
- ;; show line numbers.
+ '(backup-directory-alist `((".*" \, temporary-file-directory)))
  '(global-display-line-numbers-mode t)
- ;; save back up files to temporary directories.
- '(backup-directory-alist `((".*" . ,temporary-file-directory))))
+ '(org-log-done 'time)
+ '(org-log-into-drawer "LOGBOOK")
+ '(package-selected-packages
+   '(sml-mode ycmd solarized-theme rust-mode rainbow-delimiters markdown-mode magit company cmake-mode)))
+
+;; save org clock history across sessions
+(setq org-clock-persist 'history)
+(org-clock-persistence-insinuate)
+
+;; set org-mode default capture note file
+(setq org-default-notes-file (concat org-directory "/note.org"))
 
 ;; remap keys for iTerm 2
 ;; use cat -v or ctrl-v to show the sequence in iTerm 2.
@@ -32,3 +47,9 @@
 (define-key input-decode-map "\e[1;9D" [M-left])
 (define-key input-decode-map "\e[1;10C" [M-S-right])
 (define-key input-decode-map "\e[1;10D" [M-S-left])
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
