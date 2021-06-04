4bd8fc5d2fecb07b7ee0f1bd7d44fd1a31ceba9b
Gosin <canling0@gmail.com>
2021-06-04 23:11:15 +0100

    Use separate custom file to config emacs

---
+++ Emacs/emacs-custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(apropos-sort-by-scores t)
+ '(backup-directory-alist `((".*" \, temporary-file-directory)))
+ '(global-display-line-numbers-mode t)
+ '(org-log-done 'time)
+ '(org-log-into-drawer "LOGBOOK")
+ '(package-archives
+   '(("gnu" . "https://elpa.gnu.org/packages/")
+     ("melpa" . "https://melpa.org/packages/")))
+ '(package-selected-packages
+   '(paredit sml-mode ycmd solarized-theme rust-mode rainbow-delimiters markdown-mode magit company cmake-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- Emacs/init.el
+++ Emacs/init.el
@@ -1,8 +1,8 @@
-;; Gosin's Emacs Configuration 2021/01/09
-;; add ELPA and MELPA package repos
-(setq package-archives
-      '(("gnu" . "https://elpa.gnu.org/packages/")
-	("melpa" . "https://melpa.org/packages/")))
+;; Gosin's Emacs Configuration 2021/06/04
+;; specify file path of customization
+(setq custom-file "~/.emacs.d/emacs-custom.el")
+;; load customization file
+(load custom-file)
 
 ;; user solarized dark color theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/solarized_theme")
@@ -26,19 +26,6 @@
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c c") 'org-capture)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(apropos-sort-by-scores t)
- '(backup-directory-alist `((".*" \, temporary-file-directory)))
- '(global-display-line-numbers-mode t)
- '(org-log-done 'time)
- '(org-log-into-drawer "LOGBOOK")
- '(package-selected-packages
-   '(paredit sml-mode ycmd solarized-theme rust-mode rainbow-delimiters markdown-mode magit company cmake-mode)))
-
 ;; save org clock history across sessions
 (setq org-clock-persist 'history)
 (org-clock-persistence-insinuate)
@@ -55,9 +42,3 @@
 (define-key input-decode-map "\e[1;9D" [M-left])
 (define-key input-decode-map "\e[1;10C" [M-S-right])
 (define-key input-decode-map "\e[1;10D" [M-S-left])
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
