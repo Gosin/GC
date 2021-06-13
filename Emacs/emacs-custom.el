a1f00e1745f2b43d7a84b53deaee4fb54764489f
Gosin <canling0@gmail.com>
2021-06-13 21:15:24 +0100

    Add helm package to Emacs

--- Emacs/emacs-custom.el
+++ Emacs/emacs-custom.el
@@ -18,7 +18,7 @@
    '(("gnu" . "https://elpa.gnu.org/packages/")
      ("melpa" . "https://melpa.org/packages/")))
  '(package-selected-packages
-   '(paredit sml-mode ycmd solarized-theme rust-mode rainbow-delimiters markdown-mode magit company cmake-mode))
+   '(helm paredit sml-mode ycmd solarized-theme rust-mode rainbow-delimiters markdown-mode magit company cmake-mode))
  '(standard-indent 4)
  '(tab-always-indent nil)
  '(tab-width 4))
