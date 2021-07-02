49bba36f66d5fc0b2231316d70be8e7d5b4d2467
Gosin <canling0@gmail.com>
2021-07-02 14:22:34 +0100

    Update gnutls-algorithm-priority to fix TLS connection issue

--- Emacs/emacs-custom.el
+++ Emacs/emacs-custom.el
@@ -16,6 +16,7 @@
    '("/usr/bin" "/bin" "/usr/sbin" "/sbin" "/Applications/Emacs.app/Contents/MacOS/bin-x86_64-10_14" "/Applications/Emacs.app/Contents/MacOS/libexec-x86_64-10_14" "/Applications/Emacs.app/Contents/MacOS/libexec" "/Applications/Emacs.app/Contents/MacOS/bin" "/Library/TeX/texbin" "/usr/local/bin"))
  '(fido-mode t)
  '(global-display-line-numbers-mode t)
+ '(gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
  '(ido-create-new-buffer 'always)
  '(ido-enable-flex-matching t)
  '(ido-everywhere t)
