456054240922b389e81efbfd9153f37c99a16d40
Gosin <canling0@gmail.com>
2022-02-13 19:31:24 +0000

    Use lsp-mode instead of eglot

--- Emacs/emacs-custom.el
+++ Emacs/emacs-custom.el
@@ -66,7 +66,7 @@
      ("melpa" . "https://melpa.org/packages/")
      ("nonGnuElpa" . "https://elpa.nongnu.org/nongnu/")))
  '(package-selected-packages
-   '(tree-sitter-langs tree-sitter lua-mode glsl-mode eglot ob-rust htmlize pinentry cnfonts org-bullets helm paredit sml-mode ycmd solarized-theme rust-mode rainbow-delimiters markdown-mode magit company cmake-mode))
+   '(lsp-mode tree-sitter-langs tree-sitter lua-mode glsl-mode ob-rust htmlize pinentry cnfonts org-bullets helm paredit sml-mode ycmd solarized-theme rust-mode rainbow-delimiters markdown-mode magit company cmake-mode))
  '(standard-indent 4)
  '(tab-always-indent nil)
  '(tab-width 4))
