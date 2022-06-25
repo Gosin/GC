5506c67492c927230d5ee33f1b7b5352306ae52d
Gosin <canling0@gmail.com>
2022-06-24 22:24:42 -0500

    Simplify default face settings

--- Emacs/emacs-custom.el
+++ Emacs/emacs-custom.el
@@ -76,5 +76,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "nil" :family "berkeley mono"))))
+ '(default ((t (:height 140 :width normal :family "Berkeley Mono"))))
  '(cursor ((t (:background "yellow1" :foreground "#002b36" :inverse-video t)))))
