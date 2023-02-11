f067ee0b85063ba7255ee309eb8798cec566f112
Gosin <canling0@gmail.com>
2023-02-11 15:27:30 -0600

    Update config with yas-snippets enabled

--- Emacs/updateEmacsConfig.sh
+++ Emacs/updateEmacsConfig.sh
@@ -1,3 +1,7 @@
+#!/bin/zsh
 cp ~/.emacs.d/init.el ./init.el
+echo "init.el copied"
 cp ~/.emacs.d/emacs-custom.el ./emacs-custom.el
+echo "emacs-custom.el copied"
 cp -R ~/.emacs.d/snippets/ ./snippets/
+echo "yas snippets copied"
