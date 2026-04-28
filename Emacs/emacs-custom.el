ea4a0adb055ae8b6c893f0cfc32b1cc608e9d3e7
Gosin <canling0@gmail.com>
2026-04-27 20:08:57 -0500

    Update

--- Emacs/emacs-custom.el
+++ Emacs/emacs-custom.el
@@ -45,18 +45,15 @@
      ("melpa" . "https://melpa.org/packages/")
      ("nonGnuElpa" . "https://elpa.nongnu.org/nongnu/")))
  '(package-selected-packages
-   '(consult-org-roam consult jinja2-mode posframe yasnippet-snippets
-                      yasnippet yaml with-editor wfnames vertico
-                      treesit-auto transient rust-mode
-                      rainbow-delimiters pinentry paredit
-                      org-superstar org-roam org-notify org-mac-link
-                      org-contrib org-bullets org orderless
-                      no-littering modus-themes markdown-mode
-                      magit-section magit lua-mode llama jinja2-mode
-                      js2-mode inheritenv helm-core helm gptel
-                      flymake-ruff flymake exec-path-from-shell envrc
-                      emacsql ef-themes cond-let company cmake-mode
-                      clang-format async))
+   '(async clang-format cmake-mode company cond-let consult
+           consult-org-roam ef-themes emacsql envrc
+           exec-path-from-shell flymake flymake-ruff gptel helm
+           helm-core inheritenv jinja2-mode js2-mode llama lua-mode
+           magit magit-section markdown-mode modus-themes no-littering
+           orderless org org-bullets org-contrib org-mac-link
+           org-notify org-roam org-superstar paredit pinentry posframe
+           rainbow-delimiters rust-mode transient treesit-auto vertico
+           wfnames with-editor yaml yasnippet yasnippet-snippets))
  '(standard-indent 4)
  '(tab-always-indent nil)
  '(tab-width 4)
